c673287f24d8f739ebba48d3ea329512e32ea73c
Aleksander Kalin Nadir Hammo <akn.ham@gmail.com>
2022-05-03 10:05:25 +1000

    routes

--- mental-health-app/src/App.js
+++ mental-health-app/src/App.js
@@ -2,13 +2,29 @@ import React from "react";
 import SignUpForm from "./components/SignUpForm";
 import Form from "./components/Form";
 import "./App.css";
+import {Routes, Route, Link} from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
+    
     <div className="App">
-      <Form />
-            
+        
+      <div className="Nav">    
+        <Link to="/">Home</Link>
+        <Link to="/form">Sign Up</Link>
+        <Link to="/">Sign Up</Link>
+      </div>
+
+    <Routes>    
+        
+        <Route path='/form' element={<Form/>}/>  
+
+        
+    </Routes>
+
     </div>
+
   );
 }
 
---
+++ mental-health-app/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className='NavbarItems'>
+                <h1 className='navbar-logo'>React</h1>
+                <div className='menu-icon'>
+                    i
+                </div>
+            </nav>
+        )
+    }
+
+}
+
+
+
+export default NavBar
