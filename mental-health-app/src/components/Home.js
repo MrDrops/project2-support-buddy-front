41e9cd388a832eeb66321de5d9cc98199c629ad9
Brany Flores Gil <100183837+branyflores@users.noreply.github.com>
2022-05-03 20:58:25 +1000

    changes

--- mental-health-app/src/components/Home.js
+++ mental-health-app/src/components/Home.js
@@ -1,21 +1,28 @@
 import React from "react";
-import Checkbox from "./Checkbox";
+
+// import Checkbox from "./Checkbox";
 
 
 const Home = () => {
     
-return(
-    <div>
-   <br></br><Checkbox /> Swimming
-   
-   <br></br><br></br><Checkbox /> Hang out with friends
-
-   <br></br><br></br><Checkbox /> kill myself with css shit 
-   
-
+return (
+    <div className="home-container">
+        hello
     </div>
 )
 
- 
 };
 export default Home
+
+
+
+
+//     <div>
+//    <br></br><Checkbox /> Swimming
+   
+//    <br></br><br></br><Checkbox /> Hang out with friends
+
+//    <br></br><br></br><Checkbox /> kill myself with css shit 
+   
+
+//     </div>
