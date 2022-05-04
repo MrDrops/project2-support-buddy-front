b08f3dfd27eb0c86d103e726bc01a8d4a9bceaf3
Brany Flores Gil <100183837+branyflores@users.noreply.github.com>
2022-05-04 11:51:40 +1000

    new cards

--- mental-health-app/src/components/Home.js
+++ mental-health-app/src/components/Home.js
@@ -10,15 +10,15 @@ function Home () {
            <div className="wrapper">
            <Cards img="https://www.kindpng.com/picc/m/616-6167538_run-icon-circle-hd-png-download.png"
            title="Keep Fit"
-           description=" I could go for a quick run today" />
+           description=" I could go for a quick run today and now and after and another time ohh noooo" />
 
             <Cards img="https://thumbs.dreamstime.com/b/big-open-clapper-board-movie-reel-cinema-icon-set-movie-film-elements-flat-design-cinema-movie-time-flat-icons-f-95500226.jpg"
            title="To the movies"
-           description=" I coul have some PopCorn? Why not?!" />
+           description=" I could have some PopCorn? Why not?what about later !" />
 
            <Cards img="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Round_Landmark_Icon_Park.svg/1024px-Round_Landmark_Icon_Park.svg.png"
            title="what a view"
-           description="Check the weather, is it a nice day?" />
+           description="Check the weather, is it a nice day?lets just stay home i dotn want to do nothing i want to eat" />
            </div>
        </div>
 
