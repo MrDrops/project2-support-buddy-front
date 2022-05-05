7200610ca7ce7a508a2af9c3c85f1c1135d199b8
Brany Flores Gil <100183837+branyflores@users.noreply.github.com>
2022-05-05 10:44:07 +1000

    new cards

--- mental-health-app/src/components/Home.js
+++ mental-health-app/src/components/Home.js
@@ -21,15 +21,16 @@ function Home () {
            <div className="wrapper">
            <Cards img="https://www.kindpng.com/picc/m/616-6167538_run-icon-circle-hd-png-download.png"
            title="Keep Fit"
-           description=" I could go for a quick run today and now and after and another time ohh noooo" />
+           description="“It never gets easier, you just get better!”" />
+           
 
-            <Cards img="https://thumbs.dreamstime.com/b/big-open-clapper-board-movie-reel-cinema-icon-set-movie-film-elements-flat-design-cinema-movie-time-flat-icons-f-95500226.jpg"
-           title="To the movies"
-           description=" I could have some PopCorn? Why not?what about later !" />
+            <Cards img="https://icon-library.com/images/productivity-icon-png/productivity-icon-png-15.jpg"
+           title="Be productive"
+           description="“Action is the foundational key to all success!”" />
 
-           <Cards img="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Round_Landmark_Icon_Park.svg/1024px-Round_Landmark_Icon_Park.svg.png"
-           title="what a view"
-           description="Check the weather, is it a nice day?lets just stay home i dotn want to do nothing i want to eat" />
+           <Cards img="https://cdn-icons-png.flaticon.com/128/4207/4207229.png"
+           title="Shappen my hobbie"
+           description="“Engage in tasks and hobbies that bring you joy, like reading a book or going for a run!”" />
            </div>
        </div>
 
