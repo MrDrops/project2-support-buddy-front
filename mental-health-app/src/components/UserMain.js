3c0f82109ea3029b4d17c0f3e692d49e137047e8
Delong Yang <delongy0320@gmail.com>
2022-05-06 00:37:48 +1000

    user page

--- mental-health-app/src/components/UserMain.js
+++ mental-health-app/src/components/UserMain.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import CheckStatusMessage from './CheckStatusMessage';
+import CompleteMessage from './CompleteMessage';
+import Journal from './Journal';
+import PopUpMessage from './PopUpMessage';
+
+
 
 const UserMain = () => {
+    
+    
+    
+   
 
     return (
        <div className="wrapper">
 
             <h1>User Page</h1>
-
+            <PopUpMessage />
+            <CheckStatusMessage />
+            <CompleteMessage />
+            <Journal />
         </div>
    
     );
