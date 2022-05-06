0ae074e645c8cc6ded389528cc7706dba248eb71
Delong Yang <delongy0320@gmail.com>
2022-05-06 10:55:18 +1000

    button

--- mental-health-app/src/components/UserMain.js
+++ mental-health-app/src/components/UserMain.js
@@ -18,15 +18,19 @@ const UserMain = () => {
             <h1>User Page</h1>
             <PopUpMessage />
             <CheckStatusMessage />
-            <CompleteMessage />
-            <Journal />
+            
+
+
             <Cards img="https://cdn.imgbin.com/24/16/21/imgbin-sport-fitbit-activity-tracker-physical-exercise-computer-icons-fitbit-S8HN4FK9qdNPpBYfGaYTzzAnc.jpg"
                 title="One more step"
-                description="“Keep moving forward. If you want to fly, you have to give up what weighs you down.”" />
+                description="“Keep moving forward. If you want to fly, you have to give up what weighs you down.”" 
+                />
+             <CompleteMessage />
 
-          <Cards img="https://thumbs.dreamstime.com/b/aqua-drinking-water-vector-icon-which-can-easily-edit-aqua-drinking-water-vector-icon-which-can-easily-edit-153442810.jpg"
-         title="Drink Water"
-         description="“Drink your way to better helth!”" />
+            <Cards img="https://thumbs.dreamstime.com/b/aqua-drinking-water-vector-icon-which-can-easily-edit-aqua-drinking-water-vector-icon-which-can-easily-edit-153442810.jpg"
+            title="Drink Water"
+            description="“Drink your way to better helth!”" />
+            <CompleteMessage />
         </div>
 
     );
